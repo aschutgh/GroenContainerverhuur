b2f75491c045865bf1b443feceaa6a20df0833c8
Arthur Schut <aschutgh@gmail.com>
2018-06-05 18:56:48 +0200

    Add project files.

---
+++ GroenContainerverhuur/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GroenContainerverhuur
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
